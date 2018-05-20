c3f8c08237f4b369cc59c7e5b224ca514d304105
abenjamin1313 <abenjamin1313@gmail.com>
2018-05-20 17:54:58 -0400

    Updated properties controller. Updated edit and index views.

--- application/controllers/Properties.php
+++ application/controllers/Properties.php
@@ -13,35 +13,45 @@ class Properties extends CI_Controller {
 		$data['user_name'] = 'Adam';
 		$data['status_group'] = ['All', 'Available', 'Unavailable'];
 		$data['properties'] = $this->Property->all();
+
+		$data['selected_filter'] = $this->session->selected_filter;
+
 		$this->load->view('Layouts/header');
 		$this->load->view('Layouts/header_nav');
 		$this->load->view('properties/index', $data);
 		$this->load->view('Layouts/footer');
 	}
 
+	public function set_filter() 
+	{
+		$seesion_data['selected_filter'] = $this->input->get('filter');
+		$this->session->set_userdata($session_data);
+		redirect('properties/index');
+	}
+
 	public function show($id) 
 	{
 		$data['id'] = $id;
 		$data['name'] = $this->Property->get();
-		$version = $this->Property->get_version();
-		$data['version'] = $version->conn_id->server_info;
+		//$version = $this->Property->get_version();
+		//$data['version'] = $version->conn_id->server_info;
 		$this->load->view('properties/show', $data);
 	}
 
 	public function edit($id)
-   {
+   	{
 
-      if($_POST)
-      {
+		if($_POST)
+		{
 
-         $name = $this->input->post('name');
-         $description = $this->input->post('description');
-         $new_data['name'] = $name;
-         $new_data['description'] = $description;
+		$name = $this->input->post('name');
+		$description = $this->input->post('description');
+		$new_data['name'] = $name;
+		$new_data['description'] = $description;
 
-         $this->Property->update($id, $new_data);
-         redirect('properties/index');
-      }
+		$this->Property->update($id, $new_data);
+		redirect('properties/index');
+		}
 
       $data['property'] = $this->Property->get($id);
       $this->load->view('layouts/header');
--- application/views/properties/index.php
+++ application/views/properties/index.php
@@ -1,41 +1,50 @@
-<div class="row">
-  <br/><br />
-  <div class="col-sm-12">
-    <h2 class="text-center">Welcome, <?php echo $user_name; ?></h2>
-  </div>
-  <div class="col-sm-10 col-sm-offset-1">
-  <select>
-        <?php foreach($status_group as $status){ ?>
-          <option><?php echo $status; ?></option>
-          <?php } ?>
-    </select>
-    <br/>
-      <h3>Properties details</h3>
-      <table class="table table-striped table-hover">
-        <tr>
-          <td>IMAGE</td>
-          <td>NAME</td>
-          <td>LOCATION</td>
-          <td>STATUS</td>
-          <td>View</td>
-          <td>Edit</td> 
-        </tr>
-        <?php foreach($properties as $property) { ?>
-        <tr>
-          <td><img src="<?php echo base_url("assets/images/{$property['image']}"); ?>" width="150" /></td>  
-          <td><?php echo $property['name'] ?></td>
-          <td><?php echo $property['city'] ?>, <?php echo $property['state'] ?></td>
-          <td><?php echo $property['status_id'] ?></td>
-          <td>
-            <a href="<?php echo site_url('properties/show/' . $property['id']); ?>" type="button" class="btn btn-success">View Details</a>
-          </td>
-          <td>
-          <a href="<?php echo site_url('properties/edit/' . $property['id']); ?>" type="button" class="btn btn-info">Edit Details</a>
-         </td>
-        </tr>
-        <?php } ?>
-      </table>
+<div class="container">
+  <div class="row">
+    <br/><br />
+    <div class="col-sm-12">
+      <h2 class="text-center">Welcome, <?php echo $user_name; ?></h2>
+    </div>
+    <div class="col-sm-12">
+      <?php if(empty($selected_filter)) { ?>
+      <form method="get" action="<?php echo site_url('properties/set_filter'); ?>">
+        <select class="btn btn-default dropdown-toggle" name="filter">
+            <?php foreach($status_group as $status){ ?>
+              <option><?php echo $status; ?></option>
+              <?php } ?>
+        </select>
+        <input class="btn btn-warning" type="submit" value="Select" />
+      </form>
+            <?php } else { ?>
+              <h4>Showing filter for: <?php echo $selected_filter ?></h4>
+            <?php } ?>
       <br/>
+        <h3>Properties details</h3>
+        <table class="table table-striped table-hover">
+          <tr>
+            <td>IMAGE</td>
+            <td>NAME</td>
+            <td>LOCATION</td>
+            <td>STATUS</td>
+            <td>View</td>
+            <td>Edit</td> 
+          </tr>
+          <?php foreach($properties as $property) { ?>
+          <tr>
+            <td><img src="<?php echo base_url("assets/images/{$property['image']}"); ?>" width="150" /></td>  
+            <td><?php echo $property['name'] ?></td>
+            <td><?php echo $property['city'] ?>, <?php echo $property['state'] ?></td>
+            <td><?php echo $property['status_id'] ?></td>
+            <td>
+              <a href="<?php echo site_url('properties/show/' . $property['id']); ?>" type="button" class="btn btn-success">View Details</a>
+            </td>
+            <td>
+            <a href="<?php echo site_url('properties/edit/' . $property['id']); ?>" type="button" class="btn btn-info">Edit Details</a>
+          </td>
+          </tr>
+          <?php } ?>
+        </table>
+        <br/>
+    </div>
   </div>
 </div>
 
